4ff5791f71cf3391f97ef2c02414fab828348a1f
Shane Krueger <shane@acdmail.com>
2023-04-20 17:27:44 -0400

    Set lib and jest globals (#10)
    * Set lib es2015
    
    * Add fetch to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  roots: ['<rootDir>/test', '<rootDir>/src'],
-  testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)',
-  ],
+  roots: ["<rootDir>/test", "<rootDir>/src"],
+  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
   testEnvironment: "jsdom",
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      './config/fileTransformer.js',
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "./config/fileTransformer.js",
   },
   globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json'
-    }
-  }
-}
+    fetch: global.fetch,
+    Request: global.Request,
+    Response: global.Response,
+    AbortController: global.AbortController,
+    AbortSignal: global.AbortSignal,
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+};
