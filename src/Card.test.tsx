6ac0e890054ae083052425d47fcbbd1e78917e00
Shane32 <shane@acdmail.com>
2023-04-20 00:31:42 -0400

    Update

--- src/Card.test.tsx
+++ src/Card.test.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-//import { render, unmountComponentAtNode } from "react-dom";
 import { render, screen } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import pretty from "pretty";
